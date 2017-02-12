653ff2092766c88f62977c9af218c97eb83a1bb7
shouldsee <shouldsee.gem@gmail.com>
2017-02-12 15:44:36 +0000

    ongoing expt

--- main.m
+++ main.m
@@ -1,26 +1,36 @@
 % rule=
+itnum=1;
+[phtaux]=init_auxiliary(itnum);
+phtauxsq=square(phtaux);
 rule=randi([0 1],[1 2^3]);
-Ks=linspace(-0,10,50);
+Ks=linspace(-5,5,20);
+Kauxs=linspace(-5,5,20);
+[Ks,Kauxs]=ndgrid(Ks,Kauxs);
+Kss=Ks(:);
+Kauxss=Kauxs(:);
 Es=[];
+%%
 for rnum=(1:2^8)-1
     rule=str2num(dec2base(rnum,2,8)')';
-r10=sum(rule.*(2.^(7:-1:0)));
+    rule=randi([0 1],[1 2^3]);
+    r10=sum(rule.*(2.^(7:-1:0)));
 
 [ph1,ph0p,ph0]=init_P(rule);
 phm=ph1;
-for i=1:1;
+
+for i=1:itnum;
 phm=mergeblock(phm,ph1,ph0p,@plus);
 end
 phmt=merge2torus(phm,ph0,@plus);
 phmsq=square(phm);
 phmtsq=square(phmt);
 
-% Es=0.*Ks;
-for i=1:length(Ks);
-K=Ks(i);
-ppmt=exp(-K*phmt);
+% Es=0.*Kss;
+for i=1:length(Kss);
+K=Kss(i);
+Kaux=Kauxss(i);
 
-ppmtsq=square(ppmt);
+ppmtsq=exp(-K*phmtsq-Kaux*phtauxsq);
 
 ent=sum(entropise(sum(ppmtsq,1)));
 
@@ -41,6 +51,13 @@ Es(rnum+1,i)=(ent);
 end
 %%
 % plot((Ks),log(Es))
+figure(2)
+cla
+rEs=reshape(Es(rnum+1,:),size(Ks));
+% surface(Ks,Kauxs,rEs);
+imagesc(rEs)
+caxis([0,8])
+xlabel('x')
 % ylim([1.5 2.5])
 end
 %%
@@ -48,9 +65,11 @@ clf
 imagesc(Es)
 [~,id]=sort(Es(:,end),'descend');
 rlist=0:255;
-rkd_rlist=rlist(id);
-rkd_Es=Es(id,:);
+%%
+% rkd_rlist=rlist(id);
+% rkd_Es=Es(id,:);
 imagesc(rkd_Es)
 rkd_rlist(1:25)
+rkd_rlist(200:255)
 % phi*ppmsq*psi
 % sum(phi'.*psi)
