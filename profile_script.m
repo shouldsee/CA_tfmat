b9878a8e540567c567a5eec1f6215e3138922a9c
shouldsee <shouldsee.gem@gmail.com>
2017-02-17 16:01:35 +0000

    added_2dtca_col

--- profile_script.m
+++ profile_script.m
@@ -1,14 +1,14 @@
 profile off
 profile on
 
-sys0.critname='cov_seq2_v2';
+sys0.critname='cov_seq_v0';
 % sys0.crit=@(odr,d) odr>0.4 && min(d)<0.7;;
-sys0.cirt=@(odr,d)true;
-lst0=1:2^18;
-idx=randperm(numel(lst0));
-mlst0=reshape(lst0(idx),[2^6 2^12]);
-% bmax=2^12;
-bmax=1600;
+sys0.crit=@(odr,d)true;
+% lst0=1:2^18;
+% idx=randperm(numel(lst0));
+% mlst0=reshape(lst0(idx),[2^8 2^10]);
+bmax=2^10;
+% bmax=1600;
 for bchi=bchi0:bmax
 fprintf('batch %d of %d\n',bchi,bmax)    
 lst=mlst0(:,bchi);
